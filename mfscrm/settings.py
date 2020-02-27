07b5794d6083492777fc7583434d482edbe42f54
rserenil20 <rserenil@unomaha.edu>
2020-02-27 13:43:55 -0600

    prep for production

--- mfscrm/settings.py
+++ mfscrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0y@pod(9mm_sari2ih#m_njam$0l-brw$1$9b*9m!io9=u$cdf'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
